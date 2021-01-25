5cf4b4e189b5edab805dcb25b998c33744920b0f
kcmodev <shchristensen3@gmail.com>
2021-01-25 14:05:57 -0600

    numpy practice

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Udemy Machine Learning Class
+
+Repo to store projects and practice files for Udemy Zero to Mastery Machine Learning course.
--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
     # axis 0 = row, axis 1 = column
-    snp_data = pd.read_csv("snp_500_data.csv")
+    snp_data = pd.read_csv('snp_500_data.csv')
     print(snp_data)
     print('*' * 50)
-    print('\tdtypes.....')
+    print('\tdtypes...')
     print(snp_data.dtypes)
     print('*' * 50)
-    print('\tinfo.....')
+    print('\tinfo...')
     print(snp_data.info())
     print('*' * 50)
-    print('\tdescribe.....')
+    print('\tdescribe...')
     print(snp_data.describe())
     print('*' * 50)
-    print('\tclose mean.....')
+    print('\tclose mean...')
     print(snp_data['Close'].mean())
+
+    plt.plot(snp_data["Close"][:50])
+    plt.xlabel('Days since start')
+    plt.ylabel('Close stock price')
+    plt.show()
+
+    print('*' * 50)
+    print('\tnp array...')
+    print(np.array(pd.read_csv('snp_500_data.csv')))
 
 
 if __name__ == '__main__':
